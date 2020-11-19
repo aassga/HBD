444fd232d8a9cc04a9a1bb9309588f22c873b18f
michael.wu <98412060@stud.sju.edu.tw>
2020-11-19 15:55:02 +0800

    #底部選單元件式完成 #發布備取版_v1.0

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+  #app
+    headerbox
+    articlebox
+    footerbox
+</template>
+
+<script>
+import headerbox from './components/header.vue'
+import articlebox from './components/content.vue'
+import footerbox from './components/footer.vue'
+
+export default {
+  name: 'App',
+  components: {
+    headerbox,
+    articlebox,
+    footerbox
+  },
+} 
+</script>
+<style lang="sass" scoped>
+</style>
